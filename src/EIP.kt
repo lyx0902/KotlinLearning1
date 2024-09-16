2342938fd99660a259917cef931d631174ebe6b2
lyx0902 <320979308@qq.com>
2024-09-16 16:55:06 +0800

    initialize

--- src/EIP.kt
+++ src/EIP.kt
@@ -2,4 +2,7 @@ fun main(){
     val student = StudentFast("Alice", 20)
     println(student.getName())
     println(student.getAge())
+    student.setName("王刚")
+    println(student.getName())
+    student.hello()
 }//Encapsulation, inheritance, polymorphism
--- src/StudentFast.kt
+++ src/StudentFast.kt
@@ -1,11 +1,16 @@
-class StudentFast(private var name: String, private var age: Int) {
+class StudentFast(private var name: String, private var age: Int) {//权限控制符
     fun getName(): String = name
     fun getAge(): Int = age
     //getter and setter
     fun setName(name: String) {
+        if(name.contains("刚"))
+            return //return nothing
         this.name = name
     }
     fun setAge(age: Int) {
         this.age = age
+    }
+    fun hello() {
+        println("Hello, I am $name, $age years old.")
     }
 }
